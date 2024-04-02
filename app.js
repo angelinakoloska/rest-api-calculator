0f15d0d403e1b2416bdfeb0e2a18bebb206c5055
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-04-02 08:43:30 +0200

    Created router files and added GET endpoints

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var addRouter = require('./routes/add');
+var subtractRouter = require('./routes/subtract');
+var multiplyRouter = require('./routes/multiply');
+var divideRouter = require('./routes/divide');
 
 var app = express();
 
@@ -22,6 +26,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/add', addRouter);
+app.use('/subtract', subtractRouter);
+app.use('/multiply', multiplyRouter);
+app.use('/divide', divideRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
