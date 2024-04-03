bde3011370683b611c5e8b04eb6f96e30e79ada8
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-04-03 11:10:24 +0200

    Add authentication

--- app.js
+++ app.js
@@ -6,11 +6,13 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var authRouter = require('./routes/auth');
 var usersRouter = require('./routes/users');
 var addRouter = require('./routes/add');
 var subtractRouter = require('./routes/subtract');
 var multiplyRouter = require('./routes/multiply');
 var divideRouter = require('./routes/divide');
+var previousRouter = require('./routes/previous');
 var db = require("./models");
 db.sequelize.sync({ force: false })
 
@@ -27,11 +29,14 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/', authRouter);
 app.use('/users', usersRouter);
 app.use('/add', addRouter);
 app.use('/subtract', subtractRouter);
 app.use('/multiply', multiplyRouter);
 app.use('/divide', divideRouter);
+app.use('/previous', previousRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
