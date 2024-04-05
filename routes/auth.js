7e3691d8cc6d2fa08982de41ef65a3cec0f60fca
angelk <74609669+angelinakoloska@users.noreply.github.com>
2024-04-05 09:44:19 +0200

    Update expiration time on token

--- routes/auth.js
+++ routes/auth.js
@@ -33,7 +33,7 @@ router.post("/login", jsonParser, async (req, res, next) => {
           token = jwt.sign(
             { id: data.id, email: data.Email },
             process.env.TOKEN_SECRET,
-            { expiresIn: "20s" }
+            { expiresIn: "1h" }
           );
         } catch (err) {
           res.jsend.error("Something went wrong with creating JWT token")
